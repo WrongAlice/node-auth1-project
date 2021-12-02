ba8e4a2863dc11d1b5d593b4df187b0199b103a4
nicole pizzano <pizzano.nicole@gmail.com>
2021-12-02 11:13:46 -0500

    did more work, still not compiling

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,51 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const express = require("express");
+const router = express.Router();
+const User = require("../users/users-model");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const {restricted, checkUsernameExists, checkUsernameFree, checkPasswordLength} = require("./auth/auth-middleware");
+
+router.post("register", async (req, res) => {
+    try {
+      const hash = await bcrypt.hash(req.body.password, 10);
+      const newUser = await User.add({...req.body, password: hash});
+      req.session.user = newUser;
+      res.status(201).json(newUser);
+      } catch (error) {
+        res.status(500).json({ message: "Error registering new user" });
+        }
+        });
+      
+      
+      router.post("login", checkUsernameExists, async (req, res) =>
+      try {
+        const verifiedUser = await User.findBy({ username: req.body.username });
+        const passwordValid = await bcrypt.compare(req.body.password, user.password);
+        if (!passwordValid) {
+          res.status(401).json({ message: "Invalid Password" });
+        } else {
+          req.session.user = verifiedUser;
+          res.status(200).json(verifiedUser);
+        }
+      }
+       
+      router.get("logout", (req,res) => {
+        req.session.destroy();
+        res.status(200).json({message: "Logged out"});
+      });
+
+    
 
 
-/**
+module.exports = router;
+
+      
+      
+      
+      
+      **
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
   response:
