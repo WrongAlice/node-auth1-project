aa5e8bf8e1689a18ba7b6faa739ae3f22d7f06e0
nicole pizzano <pizzano.nicole@gmail.com>
2021-12-02 21:38:35 -0500

    new saves

--- api/server.js
+++ api/server.js
@@ -2,35 +2,39 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require("express-session");
+ const store = require('connect-session-knex')(session);
 
 const usersRouter = require("./users/users-router");
 const authRouter = require("./auth/auth-router");
+const { Store } = require("express-session");
 
 
 
 const server = express();
 
+server.use(session({
+  name: "chocolatechip",
+  secret: "supersecret",
+  saveUninitialized: false,
+  resave: false,
+  store: new Store ({
+    knex: require('knex')({
+      createTable: true,
+      clearInterval: 1000 * 60 * 60 * 24,
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: false,
+  })
+}));
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use('/api/users', usersRouter);
 server.use('/api/auth', authRouter);
-
-
-
-const sessionConfig = {
-  name: "chocolatechip",
-  secret: "supersecret",
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // 2 weeks
-    secure: false, 
-    httpOnly: true, 
-  },
-  resave: false,
-  saveUninitialized: false,
-};
-
 
 
 
